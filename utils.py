c2c197867ac781b8e7111ef7dbd0c38eb3a7efea
ProgramistHOMEP1 <tyomy4novikov@yandex.ru>
2026-04-17 15:53:17 +0300

    /info

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import os
 def get_directory_tree(path, show_hidden=False, max_depth=None, current_depth=0, prefix='', is_last_root=True):
     lines = []
     abs_path = os.path.abspath(path)
-    root_name = os.path.basename(abs_path) + '/'
+    root_name = os.path.basename(abs_path) + '\n'
 
     if current_depth == 0:
         lines.append(root_name)
@@ -22,23 +22,24 @@ def get_directory_tree(path, show_hidden=False, max_depth=None, current_depth=0,
 
     count = len(entries)
     for idx, entry in enumerate(entries):
-        full_path = os.path.join(path, entry)
-        is_last = (idx == count - 1)
+        if entry!="__pycache__":
+            full_path = os.path.join(path, entry)
+            is_last = (idx == count - 1)
 
-        connector = '└── ' if is_last else '├── '
-        lines.append(f"{prefix}{connector}{entry}{'/' if os.path.isdir(full_path) else ''}\n")
+            connector = '└── ' if is_last else '├── '
+            lines.append(f"{prefix}{connector}{entry}{'/' if os.path.isdir(full_path) else ''}\n")
 
-        if os.path.isdir(full_path):
-            new_prefix = prefix + ('    ' if is_last else '│   ')
-            subtree = get_directory_tree(
-                full_path,
-                show_hidden=show_hidden,
-                max_depth=max_depth,
-                current_depth=current_depth + 1,
-                prefix=new_prefix,
-                is_last_root=is_last
-            )
-            lines.append(subtree)
+            if os.path.isdir(full_path):
+                new_prefix = prefix + ('    ' if is_last else '│   ')
+                subtree = get_directory_tree(
+                    full_path,
+                    show_hidden=show_hidden,
+                    max_depth=max_depth,
+                    current_depth=current_depth + 1,
+                    prefix=new_prefix,
+                    is_last_root=is_last
+                )
+                lines.append(subtree)
 
     return ''.join(lines)
 
