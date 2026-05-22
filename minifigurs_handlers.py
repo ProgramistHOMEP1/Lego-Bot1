37cc7226f148ae1d2d40f91c808f2976845cdac5
ProgramistHOMEP1 <tyomy4novikov@yandex.ru>
2026-05-22 15:24:56 +0300

    chore

--- minifigurs_handlers.py
+++ minifigurs_handlers.py
@@ -98,7 +98,7 @@ async def process_name(action: Message,state: FSMContext):
 async def process_name(action,state: FSMContext):
     full_data = await state.get_data()
     # full_data['wishlist_massege'].delete() # УДАЛЕНИЕ СООБЩЕНИЯ
-    mybot.delete_message(message_id=full_data["wish_list_message_id"]chat_id=action_from_user_id)
+    mybot.delete_message(message_id=full_data["wish_list_message_id"],chat_id={action.from_user_id})
     path = (f"users_minifigures_photos/{action.from_user.id}/{full_data['wish_list_name']}")
     all_wish_list_files = (os.listdir(path))
     file_name = all_wish_list_files[int(action.text)]
